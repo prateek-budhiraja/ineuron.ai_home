6d67d788e9feb961b20b5b99bbbb8578ecf79138
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-08-01 21:14:05 +0530

    contact form styles

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 const next = document.getElementById("next");
 const prev = document.getElementById("prev");
 const cards = document.getElementsByClassName("cards");
+const mentorNext = document.getElementById("mentor-next");
+const mentorPrev = document.getElementById("mentor-prev");
+const mentorCards = document.getElementsByClassName("mentor-cards");
 
 next.addEventListener("click", (e) => {
 	cards[0].scrollBy(400, 0);
 });
 prev.addEventListener("click", (e) => {
 	cards[0].scrollBy(-400, 0);
+});
+mentorNext.addEventListener("click", (e) => {
+	mentorCards[0].scrollBy(400, 0);
+});
+mentorPrev.addEventListener("click", (e) => {
+	mentorCards[0].scrollBy(-400, 0);
 });
